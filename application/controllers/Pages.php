a54b46b59ba97351d86644100bd2e7249a86a39d
Abel Alejandro Santiago Garcia <sg.alex32@gmail.com>
2022-08-30 18:03:45 -0500

    Navegacion funcional Restaurantes antes de juntarlo con cesar

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,19 +3,18 @@ class Pages extends CI_Controller{
     public function view($page = 'login')
     {        
   
-     
+        
             if ( ! file_exists(APPPATH.'views/pages/private/'.$page.'.php') &&
             ! file_exists(APPPATH.'views/pages/public/'.$page.'.php') )
             {
-
                     // Whoops, we don't have a page for that!
                     show_404();
             }
-            if($page == 'login' || $page == 'recover'){
+            if($page === 'login' || $page === 'recover'){
                 $this->load->view('pages/public/'.$page);
             }
-            if($page == 'home' || $page == 'restaurantes' || $page == 'mesas' || 'repartidores' || 'anuncios'){
+            if($page === 'home' || $page === 'restaurantes' || $page === 'mesas' || $page === 'repartidores' || $page === 'anuncios'){
                 $this->load->view('pages/private/'.$page);
-            }    
+            }
     }
 }
