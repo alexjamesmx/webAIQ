8f69a787e3854b22b17546c2f334813b02d91804
Abel Alejandro Santiago Garcia <sg.alex32@gmail.com>
2022-08-30 13:01:00 -0500

    Navegacion funcional htaccess

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,10 +1,13 @@
 <?php
 class Pages extends CI_Controller{
-    public function view($page = 'home')
+    public function view($page = 'login')
     {        
+  
+     
             if ( ! file_exists(APPPATH.'views/pages/private/'.$page.'.php') &&
             ! file_exists(APPPATH.'views/pages/public/'.$page.'.php') )
             {
+
                     // Whoops, we don't have a page for that!
                     show_404();
             }
