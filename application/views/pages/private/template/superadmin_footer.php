421b61bed40454c88eb19e6767ae3e2fbeeab349
Abel Alejandro Santiago Garcia <sg.alex32@gmail.com>
2022-09-06 13:08:40 -0500

    erstructura lista para servicios

---
+++ application/views/pages/private/template/superadmin_footer.php
@@ -0,0 +1,32 @@
+<script src="<?= base_url() ?>static/js/vendor/jquery-3.3.1.min.js"></script>
+<script>
+    var appData = {
+        base_url: "<?= base_url() ?>",
+        user: "<?= $this->session->correo ?>",
+    };
+</script>
+<script src="<?= base_url() ?>static/js/superadmin/restaurantes.js"></script>
+<script src="<?= base_url() ?>static/js/dore.script.js"></script>
+<script src="<?= base_url() ?>static/js/scripts.js"></script>
+<script src="<?= base_url() ?>static/js/message.js"></script>
+<script src="<?= base_url() ?>static/js/home.js"></script>
+<script src="<?= base_url() ?>static/js/login.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/Chart.bundle.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/chartjs-plugin-datalabels.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/datatables.min.js"></script>
+<!-- <script src="<?= base_url() ?>static/js/vendor/select2.full.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/bootstrap-tagsinput.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/jquery.smartWizard.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/moment.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/fullcalendar.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/perfect-scrollbar.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/owl.carousel.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/progressbar.min.js"></script>
+<script src="<?= base_url() ?>static/js/vendor/mousetrap.min.js"></script> -->
+
+
+
+</body>
+
+</html>
