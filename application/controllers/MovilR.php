fd16039f1e23c49636c60ddb4c9eeed2ea82950a
Javier Rodríguez <xavilk2.0@gmail.com>
2022-09-06 13:05:25 -0500

    Actualización del servicio MovilR (getBuscadorPalabra)

--- application/controllers/MovilR.php
+++ application/controllers/MovilR.php
@@ -20,4 +20,24 @@ class MovilR extends CI_Controller {
         $resultado = $this->MovilR_model->getMenu($id);
         echo json_encode($resultado);
     }
+
+    public function getRepartidor()
+    {   
+        $resultado = $this->MovilR_model->getRepartidor();
+        echo json_encode($resultado);
+    }
+
+    public function getPublicidad()
+    {   
+        $resultado["Publicidad"] = $this->MovilR_model->getPublicidad();
+        echo json_encode($resultado);
+    }
+
+    public function getBuscadorPalabra($palabra = NULL)
+    {   
+        $palabra = "ha";
+        $resultado = $this->MovilR_model->getBuscadorPalabra($palabra);
+        echo json_encode($resultado);
+    }
+
 }
--- application/models/MovilR_model.php
+++ application/models/MovilR_model.php
@@ -12,9 +12,39 @@ class MovilR_model extends CI_Model
 
     public function getMenu($id)
     { 
-        $cmd = "select * from menu where id_res = $id";
+        $cmd = "SELECT * FROM menu WHERE id_res = $id";
         $query = $this->db->query($cmd);
         return ($query->result());
+    }
+
+    public function getRepartidor()
+    { 
+        $cmd = "SELECT * FROM repartidores WHERE activo = 1";
+        $query = $this->db->query($cmd);
+        return ($query->result());
+    }
+
+    public function getPublicidad()
+    { 
+        $cmd = "SELECT * FROM publicidad WHERE activo = 1";
+        $query = $this->db->query($cmd);
+        return ($query->result());
+    }
+    
+    public function getBuscadorPalabra($palabra)
+    { 
+        $cmd = "SELECT nombre FROM restaurantes WHERE nombre LIKE '%$palabra%'";
+        $cmd = "SELECT nombre, id_res FROM menu WHERE nombre LIKE '%$palabra%'";
+        $query = $this->db->query($cmd);
+        return ($query->result());
+
+        // Si busco Hamburguesa necesito:
+        //     -Buscar en la tabla menu el platillo(s), combo(s) o bebida(s) con el nombre(s) relacionado(s)
+        //         "SELECT nombre FROM menu WHERE nombre LIKE '%$palabra%'"
+        //     -Buscar su ID(s) de restaurante(s)
+        //         "SELECT nombre, id_res FROM menu WHERE nombre LIKE '%$palabra%' AND..."
+        //     -Mostrar de la tabla restaurantes el nombre(s) del restaurante(s) con el ID(s) perteneciente(s) 
+
     }
 
 }
