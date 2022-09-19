7ec392e4fcdd51749ca0f32909c5e17ec4182315
Cesar Ortiz <cesarortize94@gmail.com>
2022-09-19 11:42:08 -0500

    inicio

---
+++ application/controllers/Dasboard/Dasboard.php
@@ -0,0 +1,18 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dasboard extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Dasboard_model');
+    }
+
+    public function poraceptar()
+    {
+        
+    }
+}
+
+/* End of file Dasboard.php and path /application/controllers/Daisboard/Dasboard.php */
---
+++ application/models/Dasboard_model.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class Dasboard_model extends CI_Model 
+{
+    public function select_poraceptar()
+    {
+
+    }                        
+                        
+}
+
+
+/* End of file Dasboard_model.php and path /application/models/Dasboard_model.php */
